da3811d7c946e20b90ae5685a7c72a0b5ac48683
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2023-03-30 11:24:22 -0300

    pagina-login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
     return (
         <div className="container-login">
-            <form>
-                <input type='text'></input>
+            <form className='formulario-login'>
+                <label>Usuário</label>
+                <input className='input-login' type='text' placeholder='Insira seu usuário' />
+                <label>Senha</label>
+                <input className='input-password' type='password' placeholder='Insira sua senha' />
+                <button>Entrar</button>
+                <Link className='link-esqueceu-senha'>Esqueceu a senha?</Link>
+                <span className='aviso-cadastro'>Ainda não possui cadastro?</span>
+                <button>Cadastrar-se</button>
             </form>
         </div>
     )
