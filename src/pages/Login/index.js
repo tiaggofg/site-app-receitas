e71bcd0d18ac901e4a4474e2c7403afe7775f7d0
Tiago <tiago.godoy@proton.me>
2023-04-24 18:08:51 -0300

    Implementation of authentication and authorization initiated

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,6 +5,35 @@ import Input from '../../components/Input/index.js';
 import Label from '../../components/Label';
 
 function Login() {
+    function loginUser(e) {
+        e.preventDefault();
+        let username = e.target[0].value;
+        let password = e.target[1].value;
+        
+        let credentials = btoa(username + ':' + password);
+
+        let config = {
+            headers: {
+                'Authorization': 'Basic ' + credentials
+            }
+        }
+
+        axios.post(URL_BASE + '/authenticate', null, config)
+            .then(res => {
+                if (res.status == 200) {
+                    console.log('Credenciais válidas!')
+                    // TODO: inciar sessão para o usuário salvando as credenciais nos cookies/localstorage
+                }
+            })
+            .catch(err => {
+                let status = err.response.status;
+                if (status == 401) {
+                    console.log('Credenciais inválidas!');
+                    // TODO:retornar mensagem de erro para o usário que o usuário ou a senha estão incorretos
+                }
+            });
+    }
+
     return (
         <div className="container-login">
             <h2>Login</h2>
